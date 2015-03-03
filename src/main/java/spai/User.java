293e03cb78f79bbea033521eaf9caccc72d82d40
JosePech <jrocks169@gmail.com>
2015-03-03 13:25:13 +0100

    Update spai.TestsJPA, spai.User >> tomado del codigo ejemplo

--- src/main/java/spai/User.java
+++ src/main/java/spai/User.java
@@ -1,5 +1,56 @@
 package spai;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+@Entity
 public class User {
+    
+    @Id
+    private Integer id;
+
+    private String name;
+    
+    @Transient //No persistente
+    private String volatil;
+
+    public User(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    
+    public User() {
+        this(0,"");
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVolatil() {
+        return volatil;
+    }
+
+    public void setVolatil(String volatil) {
+        this.volatil = volatil;
+    }
+
+    @Override
+    public String toString() {
+        return "User[" + id + ":" + name + "]";
+    }
 
 }
