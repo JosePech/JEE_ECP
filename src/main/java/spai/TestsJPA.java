292262c35ac25493c3591b0b682f9dd276fbe88b
JosePech <jrocks169@gmail.com>
2015-03-03 13:34:51 +0100

    Update spai.TestsJPA >> Corregido error en el nombre del EntityManager - "BBDD" a "VotacionApp"

--- src/main/java/spai/TestsJPA.java
+++ src/main/java/spai/TestsJPA.java
@@ -8,16 +8,16 @@ import javax.persistence.Persistence;
 
 import org.eclipse.persistence.config.PersistenceUnitProperties;
 
-import es.art83.persistence.jpa.User1;
+import spai.User;
 
 public class TestsJPA {
 
     public static void main(String[] args) {
-        //Se borrar y se crean las tablas
+        //Se borrar y se crean las tablas 
         Map<String, String> properties = new HashMap<>();
         properties.put(PersistenceUnitProperties.DDL_GENERATION,
                 PersistenceUnitProperties.DROP_AND_CREATE);
-        EntityManager em = Persistence.createEntityManagerFactory("BBDD", properties)
+        EntityManager em = Persistence.createEntityManagerFactory("VotacionApp", properties)
                 .createEntityManager();
         
         User u1 = new User(1, "Sou u1");
