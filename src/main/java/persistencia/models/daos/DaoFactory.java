be73b4b181582ce8098c41fc8f43a22b405247ea
JosePech <jrocks169@gmail.com>
2015-03-04 11:05:22 +0100

    Add Class DaoFactory >> Tomado del ejemplo

---
+++ src/main/java/persistencia/models/daos/DaoFactory.java
@@ -0,0 +1,17 @@
+package persistencia.models.daos;
+
+public abstract class DaoFactory {
+    public static DaoFactory factory = null;
+
+    public static void setFactory(DaoFactory factory) {
+        DaoFactory.factory = factory;
+    }
+
+    public static DaoFactory getFactory() {
+        assert factory != null;
+        return factory;
+    }
+
+    public abstract TemaDAO getTemaDao();
+
+}
