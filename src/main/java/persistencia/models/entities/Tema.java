58046db439d927c42d11fd90bea1d005b56329a5
JosePech <jrocks169@gmail.com>
2015-03-04 10:53:47 +0100

    Add Class Tema

---
+++ src/main/java/persistencia/models/entities/Tema.java
@@ -0,0 +1,45 @@
+package persistencia.models.entities;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class Tema {
+    
+    public static final String TABLE = "temas";
+
+    public static final String ID = "ID";
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    public static final String NOMBRE = "NOMBRE";
+    private String nombre;
+
+    public static final String PREGUNTA = "PREGUNTA";
+    private String pregunta;
+    
+    public void setId(Integer id){
+        this.id = id;
+    }
+    
+    public Integer getId(){
+        return this.id;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public String getNombre(){
+        return this.nombre;
+    }
+    
+    public void setPregunta(String pregunta){
+        this.pregunta = pregunta;
+    }
+    
+    public String getPregunta(){
+        return this.pregunta;
+    }
+
+}
