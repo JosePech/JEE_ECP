686295102af23b64743a123bfaa9201b89751048
JosePech <jrocks169@gmail.com>
2015-03-04 11:18:36 +0100

    Add Class DaoJdbcFactory >> Tomada de ejemplo

---
+++ src/main/java/presistencia/models/daos/jdbc/DaoJdbcFactory.java
@@ -0,0 +1,58 @@
+package presistencia.models.daos.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.apache.logging.log4j.LogManager;
+
+import persistencia.models.daos.DaoFactory;
+import persistencia.models.daos.TemaDAO;
+import persistencia.models.entities.Tema;
+
+public class DaoJdbcFactory extends DaoFactory {
+    
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+
+    private static final String URL = "jdbc:mysql://localhost:3306/miwjee";
+
+    private static final String USER = "root";
+
+    private static final String PASS = "";
+
+    private static final String DROP_TABLE = "DROP TABLE IF EXISTS %s";
+
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        try {
+            if (connection == null || !connection.isValid(0)) {
+                Class.forName(DRIVER);
+                connection = DriverManager.getConnection(URL, USER, PASS);
+            }
+        } catch (ClassNotFoundException e) {
+            LogManager.getLogger(DaoJdbcFactory.class).error(
+                    "Problemas con el driver: " + e.getMessage());
+        } catch (SQLException e) {
+            LogManager.getLogger(DaoJdbcFactory.class).error(
+                    "Problemas con la BD: " + e.getMessage());
+        }
+        return connection;
+    }
+
+    public static void dropAndCreateTables() {
+        try {
+            Statement statement = getConnection().createStatement();
+            statement.executeUpdate(String.format(DROP_TABLE, Tema.TABLE));
+            statement.executeUpdate(TemaDAOJdbc.sqlToCreateTable());
+        } catch (SQLException e) {
+            LogManager.getLogger(DaoJdbcFactory.class).error("Drop tables: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public TemaDAO getTemaDao() {
+        return new TemaDAOJdbc();
+    }
+}
