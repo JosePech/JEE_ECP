919267521b3962a8ebd4a0d7146720d1f4bfd3ce
JosePech <jrocks169@gmail.com>
2015-03-18 23:16:31 +0100

    Update Class AgregarTemaBean >> Mover creacion de objetos del bean al dispatcher

--- src/main/java/vistas/AgregarTemaBean.java
+++ src/main/java/vistas/AgregarTemaBean.java
@@ -1,36 +1,42 @@
 package vistas;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+
 import org.apache.logging.log4j.LogManager;
 
 import controladores.ControllerFactory;
 import persistencia.models.entities.Tema;
 
+@ManagedBean
 public class AgregarTemaBean {
-
-    private Tema tema;
     
+    @ManagedProperty(value = "#{controllerFactoryEjbs}")
     private ControllerFactory controller;
     
-    private boolean result;
+    private int result;
     
     public static final String PATH_AGREGAR_TEMA = "agregarTema";
+
+    private String temaNombre;
+    
+    private String temaPregunta;
 
     public AgregarTemaBean(ControllerFactory controller) {
         this.controller = controller;
     }
-
-    public void setTema(Tema tema) {
-        this.tema = tema;
-    }
+    
+    public AgregarTemaBean() {
+    } 
     
     public String process(){
         try{
             this.setResult(validarDatos());
-            if(this.getResult()){
-                controller.getAgregarTemaController().agregar(tema);
+            if(this.getResult() == 1){
+                controller.getAgregarTemaController().agregar(new Tema(getTemaNombre(), getTemaPregunta()));
             }
         }catch(Exception e){
-            this.setResult(false);
+            this.setResult(-1);
             LogManager.getLogger(AgregarTemaBean.class).debug(e.getMessage());
         }
         this.resetBean();
@@ -38,26 +44,49 @@ public class AgregarTemaBean {
     }
     
     private void resetBean(){
-        this.setTema(null);
+        this.setTemaNombre(null);
+        this.setTemaPregunta(null);
     }
     
-    private boolean validarDatos(){
-        result = false;
-        if(tema != null){
-            if(tema.getNombre() != null)
-                result = tema.getNombre().length() > 0;
-            if(tema.getPregunta() != null)
-                result = tema.getPregunta().length() > 0;
-        }
-        return result;
+    private int validarDatos(){
+        int resultado = -1;
+        if(this.getTemaNombre() != null)
+            resultado = this.getTemaNombre() .length() > 0 ? 1 : -1;
+        if(this.getTemaPregunta() != null)
+            resultado = this.getTemaPregunta().length() > 0? 1 : -1;
+        return resultado;
     }
     
-    private void setResult(boolean result){
+    private void setResult(int result){
         this.result = result;
     }
     
-    public boolean getResult(){
+    public int getResult(){
         return this.result;
+    }
+    
+    public ControllerFactory getController() {
+        return controller;
+    }
+
+    public void setController(ControllerFactory controller) {
+        this.controller = controller;
+    }
+
+    public String getTemaNombre() {
+        return temaNombre;
+    }
+
+    public void setTemaNombre(String temaNombre) {
+        this.temaNombre = temaNombre;
+    }
+
+    public String getTemaPregunta() {
+        return temaPregunta;
+    }
+
+    public void setTemaPregunta(String temaPregunta) {
+        this.temaPregunta = temaPregunta;
     }
 
 }
